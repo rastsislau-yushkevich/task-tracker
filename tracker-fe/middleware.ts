be18efd8f95c8746e26b0cb06dfa149c50a135e5
Ryu <rastsislauyushkevich@gmail.com>
2025-06-16 02:27:45 +0300

    removed auth

--- tracker-fe/middleware.ts
+++ tracker-fe/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/', '/faq', '/terms-of-service'] };
+// export const config = { matcher: ['/', '/faq', '/terms-of-service'] };
