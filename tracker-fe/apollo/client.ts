1a5f30c06e449071a1131a8c2adbc9cec6357ba1
Ryu <rastsislauyushkevich@gmail.com>
2025-06-16 02:24:53 +0300

    added headers

--- tracker-fe/apollo/client.ts
+++ tracker-fe/apollo/client.ts
@@ -4,6 +4,9 @@ const createApolloClient = () => {
 	return new ApolloClient({
 		uri: process.env.NEXT_PUBLIC_API_URL,
 		cache: new InMemoryCache(),
+		headers: {
+			'x-hasura-ddn-token': process.env.DDN_TOKEN!,
+		},
 	});
 };
 
